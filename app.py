4cc66f4f2b17f0a2037747dc74d3b023c21b86e7
Jen <124745888+jenstem@users.noreply.github.com>
2024-10-29 17:18:57 -0400

    Add docstring

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 
 @app.route('/startGame', methods=['GET'])
 def startGame():
+    """
+    Initiates the game by selecting a word and creating corresponding blanks.
+
+    Returns:
+        jsonify:  A JSON response containing the word blanks for the selected word.
+    """
     pickWord = pickAnswer()
     blanks = createWordBlanks(pickWord)
     return jsonify(blanks)
